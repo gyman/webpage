b2e0016487542bae4fe8a6cc7337fa35560893f9
Grzegorz Kaszuba <uirapuru@tlen.pl>
2014-06-10 22:59:12 +0200

    capistrano configured

---
+++ app/config/deploy.rb
@@ -0,0 +1,28 @@
+set :application, "gyman"
+set :domain,      "#{application}.pl"
+set :deploy_to,   "/home/uirapuru/domains/gyman.pl"
+set :app_path,    "app"
+
+set :ssh_options, {
+    :forward_agent => true,
+    :auth_methods => ["publickey"],
+    :port => 59184
+}
+
+set :shared_files,      ["app/config/parameters.yml"]
+set :php_bin,           "/usr/local/php5.4/bin/php"
+
+set :repository,  "https://github.com/uirapuru/gyman.pl.git"
+set :scm,         :git
+
+set :model_manager, "doctrine"
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :keep_releases,  3
+
+set :use_sudo,  false
+
+# Be more verbose by uncommenting the following line
+logger.level = Logger::MAX_LEVEL
